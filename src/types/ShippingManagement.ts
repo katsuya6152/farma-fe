088f38b47a42c56d3b645e7a420da5657ddebd03
katsuya-s <katsuya.6152@gmail.com>
2024-06-19 22:33:18 +0900

    feat:Add table & Sidebar

---
+++ src/types/ShippingManagement.ts
@@ -0,0 +1,18 @@
+export type Shipping = {
+    id: string
+    name: string
+    father: string
+    mothersFather: string
+    mothersGrandfather: string
+    grandmothersGrandfather: string
+    matingDate: string
+    expectedBirthDate: string
+    birthDate: string
+    auctionDate: string
+    weight: number
+    daysOld: number
+    sex: string
+    price: number
+    buyer:string
+    memo: string
+}
