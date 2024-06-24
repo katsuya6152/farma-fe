bdcd8150fdad3c21e1ae88d5f57beb3b162d0699
katsuya-s <katsuya.6152@gmail.com>
2024-06-24 11:47:56 +0900

    fix:Fix BASE_URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import { Shipping } from "@/types/ShippingManagement";
 
-// const BASE_URL = "https://farma-be.ka28.workers.dev";
-const BASE_URL = "http://127.0.0.1:8787";
+const BASE_URL = "https://farma-be.ka28.workers.dev";
+// const BASE_URL = "http://127.0.0.1:8787";
 
 export async function getShippingData() {
   const res = await fetch(`${BASE_URL}/api/shipping`, {
