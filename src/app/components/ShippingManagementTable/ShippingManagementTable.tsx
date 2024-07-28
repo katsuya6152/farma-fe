7fab0a0e8c395a3e8a49e142ec1c0af0f0f45f5c
katsuya6152 <katsuya.6152@gmail.com>
2024-07-28 20:34:34 +0900

    fix

--- src/app/components/ShippingManagementTable/ShippingManagementTable.tsx
+++ src/app/components/ShippingManagementTable/ShippingManagementTable.tsx
@@ -52,7 +52,7 @@ export function ShippingManagementTable<TData, TValue>({
                         ? null
                         : flexRender(
                             header.column.columnDef.header,
-                            header.getContext()
+                            header.getContext(),
                           )}
                     </TableHead>
                   );
@@ -71,7 +71,7 @@ export function ShippingManagementTable<TData, TValue>({
                     <TableCell key={cell.id}>
                       {flexRender(
                         cell.column.columnDef.cell,
-                        cell.getContext()
+                        cell.getContext(),
                       )}
                     </TableCell>
                   ))}
